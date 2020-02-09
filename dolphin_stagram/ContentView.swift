db4a8ef78fd319d40de9d8470d986dd2bb21728a
park chanyoung <pcx474@gmail.com>
2020-02-09 21:21:52 +0900

    post

--- dolphin_stagram/ContentView.swift
+++ dolphin_stagram/ContentView.swift
@@ -65,6 +65,8 @@ struct ContentView_Previews: PreviewProvider {
 struct Home : View{
     
     @ObservedObject var observed = observer()
+    @ObservedObject var postobserved = Postobserver()
+    
     @State var show = false
     @State var user = ""
     @State var url = ""
@@ -80,9 +82,14 @@ struct Home : View{
                     }
                 }
                 
-                ForEach(0..<8){_ in
-                    postCard(user: "", image: "", id: "")
+                if postobserved.posts.isEmpty{
+                    Text("No Posts").fontWeight(.heavy)
+                }else{
+                    ForEach(postobserved.posts){i in
+                        postCard(user: i.name, image: i.image, id: i.id, likes: i.likes, comments: i.comment)
+                    }
                 }
+                
             }.sheet(isPresented: $show){
                 statusView(url : self.url,name : self.user)
             }
@@ -116,16 +123,18 @@ struct StatusCard : View{
 }
 
 struct postCard : View {
-    var user = "people"
+    var user = ""
     var image = ""
     var id = ""
+    var likes = ""
+    var comments = ""
     
     var body : some View{
         VStack(alignment: .leading, content: {
             
             HStack{
-                Image("people").resizable().frame(width: 30, height: 30)
-                Text("PCY")
+                AnimatedImage(url : URL(string: image)).resizable().frame(width: 30, height: 30)
+                Text(user)
                 Spacer()
                 Button(action : {
                     
@@ -134,7 +143,7 @@ struct postCard : View {
                 }.foregroundColor(Color("darkAndWhite"))
             }
             
-            Image("testing")
+            AnimatedImage(url : URL(string : image))
             .resizable()
                 .frame(height:350)
             
@@ -162,8 +171,8 @@ struct postCard : View {
                 }.foregroundColor(Color("darkAndWhite"))
             }.padding(.top,8)
             
-            Text("2 Likes")
-            Text("View all 3 Comments")
+            Text("\(likes) Likes")
+            Text("View all \(comments) Comments")
         }).padding(8)
     }
 }
@@ -201,6 +210,55 @@ class observer : ObservableObject{
             }
         }
     }
+}
+
+class Postobserver : ObservableObject{
+    @Published var posts = [datatype1]()
+    
+    init() {
+        let db = Firestore.firestore()
+        db.collection("posts").addSnapshotListener{ (snap,err) in
+            if err != nil{
+                print((err?.localizedDescription))
+                return
+            }
+            
+            for i in snap!.documentChanges {
+                if i.type == .added{
+                    let id = i.document.documentID
+                    let name = i.document.get("name") as! String
+                    let image = i.document.get("image") as! String
+                    let comment = i.document.get("comment") as! String
+                    let likes = i.document.get("likes") as! String
+                    
+                    
+                    self.posts.append(datatype1(id: id, name: name, image: image, comment: comment, likes: likes))
+                }
+                
+                
+                if i.type == .removed{
+                    let id = i.document.documentID
+                    
+                    for j in 0..<self.posts.count{
+                        if self.posts[j].id == id{
+                            self.posts.remove(at: j)
+                            return
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+struct datatype1 : Identifiable {
+    var id : String
+    var name : String
+    var image : String
+    var comment : String
+    var likes : String
+    
 }
 
 struct datatype : Identifiable {
