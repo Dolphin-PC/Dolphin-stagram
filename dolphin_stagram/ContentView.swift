fdcfbeb07ba3108e3ca08726a5f639bac80d6892
park chanyoung <pcx474@gmail.com>
2020-02-09 19:13:15 +0900

    Initial Commit

---
+++ dolphin_stagram/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  dolphin_stagram
+//
+//  Created by 박찬영 on 2020/02/09.
+//  Copyright © 2020 박찬영. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
